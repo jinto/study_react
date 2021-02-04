74afc5166c1d2594de25443eb9ec4efb7fc40c30
jaypark <jaypark@gmail.com>
2021-02-04 19:52:29 +0900

    이제 챕터6인데.. 재미있구나!!!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+function Movie({id, year, title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+}
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log("Hello")
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+
+  getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({movies, isLoading: false})
+    console.log(this.state.movies)
   }
   componentDidMount() {
-    console.log("didmount")
+    this.getMovies()
   }
-  componentDidUpdate() {
-    console.log("didmount")
-
-  }
-
-  state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count+1
-    }))
-  }
-  minus = () => {
-    this.setState(current => ({
-      count: current.count -1
-    }))
-  }
-
   render() {
-    console.log("render")
+    const { isLoading, movies } = this.state
     return (
-      <div>
-        <h1>숫자 올리기 내리기 { this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+      <div>{isLoading? 'Loading...': 'Ready'}
+      {
+        movies.map(movie=> 
+          <Movie id={movie.id} 
+            year={movie.year} 
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.poster}
+          />
+        )
+      }
       </div>
     )
   }
