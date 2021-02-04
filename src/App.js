b681649df99ba199196dc84b2e33992d9d2cee08
jaypark <jaypark@gmail.com>
2021-02-04 19:27:25 +0900

    다지우고 숫자 올리기 내리기 .. 오호!

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React from 'react'
 
-function Movie(props) {
-  var { fav, pic , papa } = props;
-  return (
-    <div>
-      <h3>감자감자감자 진짜? { fav } {papa}</h3>
-      <img alt="fav" width="100px" src={pic}></img>
-    </div>
-  );
-}
+class App extends React.Component {
 
-const foodLikes = [
-  {
-    name: '오코노미야키',
-    image: 'https://d20aeo683mqd6t.cloudfront.net/images/imgs/000/015/197/original/image001.jpeg?1561111369&d=750x750'
-  },
-  {
-    name: '타코야끼',
-    image: 'https://d20aeo683mqd6t.cloudfront.net/images/imgs/000/015/214/original/image006.jpeg?1561342506&d=750x750'
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState({count: this.state.count+1});
   }
-]
-function App() {
-  return (
-    <div>
-      <h1>요기에 제목을...</h1>
-      {
-        foodLikes.map(dish => (
-          <Movie fav={dish.name} pic={dish.image} />
-        ))
-      }
-    </div>
-  );
+  minus = () => {
+    this.setState({count: this.state.count-1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>숫자 올리기 내리기 { this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
