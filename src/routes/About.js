3c0da16d0c6a39a2131b58289b522b297a914f5a
jaypark <jaypark@gmail.com>
2021-02-04 21:32:44 +0900

    중간 커밋, nav 도 만들었다

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+    return (
+    <div className="about__container">
+        <span>About this page: I build it because I love movies.</span>
+        <span>Jay Park</span>
+    </div>
+    )
+}
+
+export default About
