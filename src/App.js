3c57efc60d25282b37461d5f2f079c70792bf44f
jaypark <jaypark@gmail.com>
2021-02-04 19:33:06 +0900

    숫자 올리고 내리기 완성. 이제 또 다른 걸 만든다고 다 지우라네..

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log("Hello")
+  }
+  componentDidMount() {
+    console.log("didmount")
+  }
+  componentDidUpdate() {
+    console.log("didmount")
+
+  }
 
   state = {
     count: 0,
   };
 
   add = () => {
-    this.setState({count: this.state.count+1});
+    this.setState(current => ({
+      count: current.count+1
+    }))
   }
   minus = () => {
-    this.setState({count: this.state.count-1});
+    this.setState(current => ({
+      count: current.count -1
+    }))
   }
 
   render() {
+    console.log("render")
     return (
       <div>
         <h1>숫자 올리기 내리기 { this.state.count} </h1>
