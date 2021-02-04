2bbcbc41c10c4d3c4926f9920d667eb0d693c563
jaypark <jaypark@gmail.com>
2021-02-04 20:40:56 +0900

    중간 커밋.. 영화 목록 보인다

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css'
 
 class App extends React.Component {
   state = {
@@ -19,14 +20,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state
     return (
-      <section class="container">
+      <section className="container">
       {
         isLoading ? (
-          <div class='loader'>
-            <span class="loader__text">Loading...</span>
+          <div className='loader'>
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div class='movies'>
+          <div className='movies'>
           {
             movies.map(movie=> 
               <Movie key={movie.id} 
@@ -35,6 +36,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             )
           }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css'
 
-function Movie({id, year, title, summary, poster}) {
+function Movie({year, title, summary, poster, genres}) {
   return (
-    <div class="movie__data">
-      <h3 class="movie_title">{title} - {year}</h3>
-      <p class="movie__summary">{summary}</p>
+    <div className="movie">
       <img width='100px' alt={title} src={poster}></img>
+      <h3 className="movie__title">{title}</h3>
+      <h5 className="movie__year">{year}</h5>
+      <p className="movie__summary">{summary.slice(0, 180)}</p>
+      <p className="movie__genres">{genres.map((genre, idx) => {
+        return <li key={idx} className="movie_genre">{genre}</li>})}
+      </p>
     </div>
   )
 }
@@ -16,6 +21,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 
