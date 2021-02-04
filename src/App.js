0574a72838013a556a424c14dc2995b78f496a03
jaypark <jaypark@gmail.com>
2021-02-04 18:26:30 +0900

    destructing assignment 에는 var 가 필요하네. 책나온지가 얼마나 됐다고.. 따라 칠수가 없구나

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+function Movie(props) {
+  var { fav, some , papa } = props;
+  return <h3>감자감자감자 진짜? { fav } {papa}</h3>;
+}
+
 function App() {
-  //return <div className="App" />;
-  return <div>ㅎㅏ하하하</div>;
+  return (
+    <div>
+      <h1>요기에 제목을...</h1>
+      <Movie fav='Kimchi' some={true} papa={['aa', 1, 2]} />
+    </div>
+  );
 }
 
 export default App;
